2da87ec075bbf089c34b689cce2c5889c4b3a352
Eelkevd <34510065+Eelkevd@users.noreply.github.com>
2018-01-31 09:21:42 +0100

    0.8
    From yesterday, doesn't work yet

--- api.php
+++ api.php
@@ -3,8 +3,24 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userName = $_POST["key"]; 
     $blogText = $_POST["message"];
-	$content = $userName ." ". $blogText;
-	echo $content;
+
+    $dsn = "mysql:dn=blogdb;host=127.0.0.1"
+    $user_name = "root";
+    $pass_word = "";
+
+    //$connection = new mysqli("localhost","root", "", "blog");
+    $connection = new PDO($dsn, $user_name, $pass_word)
+	//$content = $userName ." ". $blogText;
+	//echo $content;
+	try {
+		$sql = "INSERT INTO blog (message)" .
+		"VALUES ('$blogText')";
+		$connection->exec($sql);
+		  $blogText ."added to database";
+	}
+	catch(PDOException $e) {
+		echo $e->getMessage();
+	}
 }
 
 ?>
